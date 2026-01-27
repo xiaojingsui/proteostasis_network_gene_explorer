8479b1558e39286557b195b8825a3c3c4a24e5cc
Xiaojing Sui <115586445+xiaojingsui@users.noreply.github.com>
2026-01-26 21:12:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ st.markdown("""
         border-radius: 12px;
         box-shadow: 0 4px 20px rgba(0,0,0,0.08);
         width: 100%;
-        margin-top: 60px;
+        margin-top: 30px;
         border-collapse: collapse;
     }
 
