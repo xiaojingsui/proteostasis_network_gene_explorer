ef54a07950ce78b6a3e5731b7fec86a2ebe20c1d
Xiaojing Sui <115586445+xiaojingsui@users.noreply.github.com>
2026-06-29 14:12:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1209,7 +1209,8 @@ elif selected_page == "Feedback":
                 
                 evidence = st.text_area(
                     "Evidence *", 
-                    placeholder="An example for the protein TAB2: TAB2 and TAB3 bind to BECN to inhibit autophagy. TAB2 also interacts with ATG13 and is phosphorylated by ULK1.",
+                    placeholder="""An example for the protein TAB2: 
+                    TAB2 and TAB3 bind to BECN to inhibit autophagy. TAB2 also interacts with ATG13 and is phosphorylated by ULK1.""",
                     help="Include references to publications, experiments or annotations that support the information."
                 )
                 
